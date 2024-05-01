0b5fd2c78fda263b9dc16d52432be6dd8e2f8164
samhou1988 <samhou1988@gmail.com>
2024-05-01 12:08:18 +0800

    feat: path alias and shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
   return (
     <div className="flex">
+      <Button>Click me</Button>
       <h1 className="text-4xl">Hello, Next.js!</h1>
     </div>
   );
