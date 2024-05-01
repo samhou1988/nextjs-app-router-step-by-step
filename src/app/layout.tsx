90fe455732b3d4a711fc6ed8493afbecf9e9d17c
samhou1988 <samhou1988@gmail.com>
2024-05-01 12:01:49 +0800

    feat: configure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import { PropsWithChildren } from "react";
+import type { Metadata } from 'next';
 
 import './globals.css';
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme',
+    default: 'Acme', // a default is required when creating a template
+  },
+  keywords: ['react', 'web', 'framework'],
+  description: "Acme is a simple, modern, and fast web framework for React.",
+}
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html>
+    <html lang="zh-cmn-Hans">
       <body>
         <div>
           <main>{children}</main>
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const tailwindConfig: Config = {
+  content: [
+    "./src/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default tailwindConfig;
+
