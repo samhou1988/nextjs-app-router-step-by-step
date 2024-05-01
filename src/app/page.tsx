29eb2550b92089dfa179760302967f6425211bfb
samhou1988 <samhou1988@gmail.com>
2024-05-01 12:23:24 +0800

    feat: client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
+import LikeButton from '@/components/like-button';
 
 export default function Page() {
   return (
     <div className="flex">
       <Button>Click me</Button>
       <h1 className="text-4xl">Hello, Next.js!</h1>
+      <LikeButton />
     </div>
   );
 }
